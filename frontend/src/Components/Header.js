8296307f6a26174e7f6b31383deab594da13a9a7
Thu Nguyễn <ititiu20315@student.hcmiu.edu.vn>
2024-04-03 19:21:46 +0700

    Update Header.js

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -2,15 +2,15 @@ import React from "react";
 import LogoE from '../image/LogoE.png'
 const Header = () =>{
     return(
-        <div class="bg-white flex font-sans h-fit items-center font-bold text-xl">
-            <div class="float-left w-1/5">
-                <img class="mt-4 mr-3 mb-3 ml-7"
+        <div className="bg-white flex font-sans h-fit items-center font-bold text-xl">
+            <div className="float-left w-1/5">
+                <img className="mt-4 mr-3 mb-3 ml-7"
                 src={LogoE} 
                 alt="Logo" 
                 width={80}/>
             </div>
-            <div class="float-left h-24 border-l-2 my-3 ml-3"></div>
-            <div class="float-left my-3 ml-3 pl-5">
+            <div className="float-left h-24 border-l-2 my-3 ml-3"></div>
+            <div className="float-left my-3 ml-3 pl-5">
                 <h3 className="mx-3 my-3">E-checking</h3>
                 <h3 className="mx-3 my-3">Checking Attendance</h3>
             </div>
